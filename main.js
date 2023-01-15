577679300fc071a59c804e86eed7b8dd9fc08488
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2023-01-14 22:05:15 -0500

    change second flag functionality and button added

--- main.js
+++ main.js
@@ -1,45 +1,64 @@
-async function getCountries(){
-  const countriesPopulation = []
-  const response = await fetch('https://restcountries.com/v3.1/all')
-  const responseData = await response.json()
-  await responseData.forEach(el => {
+const gameButton = document.getElementById("gameBtn");
+const firstCountry = document.getElementById("firstCountry");
+const secondCountry = document.getElementById("secondCountry");
+
+
+gameButton.addEventListener('click', changeCountryTwo)
+
+async function getCountries() {
+  const countriesPopulation = [];
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  const responseData = await response.json();
+  await responseData.forEach((el) => {
     const country = {
       name: el.name.common,
       population: el.population,
-      alpha3Code: el.cca3
-    }
-    countriesPopulation.push(country)
-  })
-  return countriesPopulation
+      alpha3Code: el.cca3,
+    };
+    countriesPopulation.push(country);
+  });
+  return countriesPopulation;
 }
 
-async function getFlags(codeOne, codeTwo){
-  const firstCountry = document.getElementById('firstCountry')
-  const secondCountry = document.getElementById('secondCountry')
-  console.log(firstCountry, secondCountry);
-  const responseOne = await fetch(`https://countryflagsapi.com/svg/${codeOne}`)
-  const responseTwo = await fetch(`https://countryflagsapi.com/svg/${codeTwo}`)
+async function getFlags(codeOne, codeTwo) {
+  const responseOne = await fetch(`https://countryflagsapi.com/svg/${codeOne}`);
+  const responseTwo = await fetch(`https://countryflagsapi.com/svg/${codeTwo}`);
   firstCountry.src = `${responseOne.url}`;
   secondCountry.src = `${responseTwo.url}`;
-  
 }
 
-const countries = await getCountries()
-const numberOfCountries = countries.length - 1
+const countries = await getCountries();
+const numberOfCountries = countries.length - 1;
 
-function getARandomCountry(){
-  const randomNumber = Math.ceil(Math.random()*numberOfCountries)
-  return randomNumber
+function getARandomCountry() {
+  const randomNumber = Math.ceil(Math.random() * numberOfCountries);
+  return randomNumber;
 }
 
-const {name:countryOne, population: populationOne, alpha3Code: countryCodeOne} = countries[getARandomCountry()]
-const {name:countryTwo, population: populationTwo, alpha3Code: countryCodeTwo} = countries[getARandomCountry()]
+async function changeCountryTwo() {
+  const {
+    name: countryTwo,
+    population: populationTwo,
+    alpha3Code: countryCodeTwo,
+  } = countries[getARandomCountry()];
+  const responseTwo = await fetch(`https://countryflagsapi.com/svg/${countryCodeTwo}`);
+  console.log(`${countryOne}, ${countryCodeOne} has ${populationOne} habitants
+while ${countryTwo}, ${countryCodeTwo} has ${populationTwo} habitants`);
+  secondCountry.src = `${responseTwo.url}`;
+}
 
+const {
+  name: countryOne,
+  population: populationOne,
+  alpha3Code: countryCodeOne,
+} = countries[getARandomCountry()];
+const {
+  name: countryTwo,
+  population: populationTwo,
+  alpha3Code: countryCodeTwo,
+} = countries[getARandomCountry()];
 
 console.log(`${countryOne}, ${countryCodeOne} has ${populationOne} habitants
-while ${countryTwo}, ${countryCodeTwo} has ${populationTwo} habitants`)
+while ${countryTwo}, ${countryCodeTwo} has ${populationTwo} habitants`);
 
-getFlags(countryCodeOne, countryCodeTwo)
-
-
-
+getFlags(countryCodeOne, countryCodeTwo);
